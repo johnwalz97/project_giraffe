89ea53eab08709a5925d4f4ce683c691e90a42af
johnwalz97 <johnwalz97@gmail.com>
2015-10-28 13:31:00 -0700

    update

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -10,17 +10,69 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="/assets/css/font-awesome.css">
     <link rel="stylesheet" href="/assets/style.css">
     <style>
-        body {
+        .background {
+			position: absolute;
+			top: 0px;
+			width: 100%;
+			height: 100%;
+			z-index: 98;
             background-image: url('/assets/pics/background.jpg');
+			background-size: cover;
         }
+		.overlay {
+			position: absolute;
+			width: 100%;
+			height: 100%;
+			background: #202020;
+			opacity: .5;
+			z-index: 99;
+		}
+		.container_main1  {
+			position: absolute;
+			color: white;
+			font-size: 48px;
+			font-weight: bold;
+			width: 100%;
+			z-index: 101;
+			top: 50px;
+			width: 100%;
+			text-align: center;
+		}
+		.container_main2  {
+			position: absolute;
+			color: white;
+			width: 100%;
+			z-index: 101;
+			top: 130px;
+			width: 100%;
+			text-align: center;
+		}
+		.navbar {
+			position: absolute;
+			z-index: 100;
+			width: 100%;
+		}
     </style>
 </head>
 <body>
+<nav class="navbar navbar-carbon" style="border-bottom: solid thin black; margin-left: 0px !important; margin-top: 0px !important; border: none !important">
+    <div class="container">
+        <div class="navbar-header">
+            <span class="navbar-brand" style="color: #202020">AJ Conceptz</span>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a class="list_link" href="/mains/">Shop as a guest</a></li>
+			<li><a class="list_link" href="/welcome/contact">Contact Us</a></li>
+        </ul>
+        <ul class="nav navbar-nav pull-right">
+            <li class="pull-right"><a class="list_link" href="/admins/logoff">Log Off</a></li>
+        </ul>
+    </div>
+</nav>
 <?php
-$this->load->view('partials/nav');
 $this->load->view('partials/foot');
 ?>
-<div class="container" style="text-align: center;">
-	<h1 style="color: #202020; font-size: 48px; font-weight: bold;">Welcome to AJ Conceptz</h1>
-	<h3 style="color: white; margin-top: 50px;">Where beautiful design meets incredible performace!</h3>
-</div>
+<div class="background"></div>
+<div class="overlay"></div>
+<h1 class="container_main1">Welcome to AJ Conceptz</h1>
+<h3 class="container_main2">Where beautiful design meets incredible performace!</h3>
